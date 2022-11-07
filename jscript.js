ae3cd53fff4ba04f07a7a7e4af564fa764629832
MarieMakesMoneys <marie.collecchia@gmail.com>
2022-11-07 15:39:45 +0100

    Set up Book constructor and addBookToLibrary function

--- jscript.js
+++ jscript.js
@@ -1,1 +1,24 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.info = function() {
+        return title + " " + author + " " + pages + " pages " + read
+    }
+}
+
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false)
+const fakeBook = new Book('fake title', 'fakeauthor', 453, 'read')
+
+
+function addBookToLibrary(Book) {
+    myLibrary.push(Book)    
+}
+
+addBookToLibrary(theHobbit)
+addBookToLibrary(fakeBook)
+console.log(myLibrary)
 
